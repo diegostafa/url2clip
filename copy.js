ebd5aa66dcb1cfb0aba4e656818fa843cc8daebe
DiegoStafa <stafadeigo18@gmail.com>
2021-12-02 18:09:41 +0100

    first commit

---
+++ copy.js
@@ -0,0 +1,8 @@
+var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
